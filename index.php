88b8e7b378e8deab6630ab90907c789f60dee8dc
Sakurai <45660470+jvgrubef@users.noreply.github.com>
2025-04-03 20:27:31 -0300

    You didn't see anything 🤫

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         <meta charset="UTF-8">
         <link rel="icon" type="image/x-icon" href="img/favicon.ico">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>BIAMAC</title>
+        <title>AOM</title>
         <link rel="stylesheet" href="css/reset.css">
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/list.css">
